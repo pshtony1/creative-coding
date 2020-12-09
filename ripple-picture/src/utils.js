ed8f87459aa9dad8640d8ffe1b4ace210d5070f2
Feca <pshtony1@naver.com>
2020-12-09 18:28:38 +0900

    Ripple Picture

---
+++ ripple-picture/src/utils.js
@@ -0,0 +1,24 @@
+export function distance(x1, y1, x2, y2) {
+  const x = x2 - x1;
+  const y = y2 - y1;
+
+  return Math.sqrt(x * x + y * y);
+}
+
+export function collide(x1, y1, x2, y2, radius) {
+  if (distance(x1, y1, x2, y2) <= radius) {
+    return true;
+  }
+
+  return false;
+}
+
+export function getBWValue(red, green, blue, isReversed) {
+  const detect = 2;
+
+  if (!isReversed) {
+    return 255 - Math.floor((red + green + blue) / detect);
+  }
+
+  return Math.floor((red + green + blue) / detect);
+}
